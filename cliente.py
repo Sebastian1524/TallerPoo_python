81143099a9843486e9c6c116b4acce4e27dc13e7
Sebastian1524 <sebas15caceres@gmail.com>
2025-03-14 19:47:44 -0500

    versión 2

---
+++ cliente.py
@@ -0,0 +1,9 @@
+def tomar_datos(self):
+    self.nom_cliente = input("Digite el nombre cliente: ")
+    self.apellido_cliente = input("Digite su apellido Cliente: ")
+
+def procesar_datos(self):
+    aux = self.nombre_cliente  + " " + self.apellido_cliente
+    
+def procesar_datos(self):
+    
